5642da0e2267205f0664c4aed8c04d29cf4cd5e4
josphine001 <joseykaluu@gmail.com>
2025-05-25 11:57:30 +0300

    function commit

---
+++ calls.py
@@ -0,0 +1,24 @@
+from function import greetings
+greetings()
+
+from function import user
+user()
+
+from function import fibbonaci
+fibbonaci(10)
+
+from function import factorial_of_number
+factorial_of_number()
+
+
+
+from classes import students
+stud1 = students("henry", "doe", 45, 123456)
+
+from classes import courses
+course1 = courses("python", "cit 101", 3)
+
+from classes import library
+book1 = library("python programming", "juma doe")
+
+
